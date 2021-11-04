b8d88d269911262923d56e82348fa09fa7e6e7e1
Kandio16 <kandio1604@gmail.com>
2021-11-04 17:52:46 +0200

    stories/add-label

--- src/storybook/components/related-components/component-description-map.js
+++ src/storybook/components/related-components/component-description-map.js
@@ -1,9 +1,12 @@
 import { SplitButtonDescription } from "./descriptions/split-button-description";
 import { ButtonGroupDescription } from "./descriptions/button-group-description";
+import { LabelDescription } from "./descriptions/label-description";
 
 export const SPLIT_BUTTON = "split-button";
 export const BUTTON_GROUP = "button-group";
+export const LABEL = "label";
 
 export const descriptionTypesMap = new Map();
 descriptionTypesMap.set(SPLIT_BUTTON, <SplitButtonDescription />);
 descriptionTypesMap.set(BUTTON_GROUP, <ButtonGroupDescription />);
+descriptionTypesMap.set(LABEL, <LabelDescription />);

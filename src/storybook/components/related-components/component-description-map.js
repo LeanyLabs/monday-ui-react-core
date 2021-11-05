1583dc1adda122d60aa0b14bdd9b16f868535243
Kandio16 <kandio1604@gmail.com>
2021-11-05 12:34:34 +0200

    stories/add-checkbox

--- src/storybook/components/related-components/component-description-map.js
+++ src/storybook/components/related-components/component-description-map.js
@@ -1,9 +1,12 @@
 import { SplitButtonDescription } from "./descriptions/split-button-description";
 import { ButtonGroupDescription } from "./descriptions/button-group-description";
+import { CheckboxDescription } from "./descriptions/checkbox-description";
 
 export const SPLIT_BUTTON = "split-button";
 export const BUTTON_GROUP = "button-group";
+export const CHECKBOX = "checkbox";
 
 export const descriptionTypesMap = new Map();
 descriptionTypesMap.set(SPLIT_BUTTON, <SplitButtonDescription />);
 descriptionTypesMap.set(BUTTON_GROUP, <ButtonGroupDescription />);
+descriptionTypesMap.set(CHECKBOX, <CheckboxDescription />);

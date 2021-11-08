6a98c8b533cb1f0a262debba12c95454d5b0934d
Kandio16 <kandio1604@gmail.com>
2021-11-08 15:41:33 +0200

    chip/test/init

--- src/components/Chips/__tests__/chips.jest.js
+++ src/components/Chips/__tests__/chips.jest.js
@@ -2,7 +2,32 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Chips from "../Chips";
 
-it("renders correctly with empty props", () => {
-  const tree = renderer.create(<Chips />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe("Chips", () => {
+  describe("Snapshot Tests", () => {
+    it("renders correctly with empty props", () => {
+      const tree = renderer.create(<Chips />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders correctly with color", () => {
+      const tree = renderer.create(<Chips color={Chips.colors.NEGATIVE} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    
+    it("renders correctly disabled chip", () => {
+      const tree = renderer.create(<Chips disabled={true} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders correctly without close button", () => {
+      const tree = renderer.create(<Chips readOnly={true} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders correctly with text", () => {
+      const tree = renderer.create(<Chips label="text" />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+  });
 });

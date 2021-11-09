d5234034d35dfa577d4ae29bd02d66e993eb3fb8
Kandio16 <kandio1604@gmail.com>
2021-11-09 14:36:38 +0200

    test/add-chips-tests

--- src/components/Chips/__tests__/chips.jest.js
+++ src/components/Chips/__tests__/chips.jest.js
@@ -1,6 +1,9 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import Chips from "../Chips";
+import { render, fireEvent, screen } from "@testing-library/react";
+import { sinon, expect as sinonExpect } from "../../../test/test-helpers";
+import renderer from "react-test-renderer";
+import { Calendar } from "../../Icon/Icons";
 
 describe("Chips", () => {
   describe("Snapshot Tests", () => {
@@ -29,5 +32,40 @@ describe("Chips", () => {
       expect(tree).toMatchSnapshot();
     });
 
+    // To Do
+    it.skip("renders correctly with right icon ", () => {
+      const tree = renderer.create(<Chips rightIcon={Calendar} readOnly />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe("Integration Tests", () => { 
+    const lable = "Chip";
+    const className = "test-class";
+    let onDeletedStub;
+    let chipComponent;
+    beforeEach(() => {
+      onDeletedStub = sinon.stub();
+      chipComponent = render(
+        <Chips className={className} onDelete={onDeletedStub} label={lable}  />
+      );
+    });
+
+    it("should call the click callback when clicked", () => {
+      fireEvent.click(screen.queryByRole('button'));
+      sinonExpect(onDeletedStub).to.be.calledOnce;
+    })
+
+    it("should not have close icon when read only", () => {
+      const { container, rerender } = chipComponent;
+      rerender (<Chips readOnly />);
+      expect(container.firstChild.classList.contains('with-close')).toBeFalsy();
+    })
+
+    it("should not have close icon when disabled", () => {
+      const { container, rerender } = chipComponent;
+      rerender (<Chips disabled />);
+      expect(container.firstChild.classList.contains('with-close')).toBeFalsy();
+    })
   });
 });

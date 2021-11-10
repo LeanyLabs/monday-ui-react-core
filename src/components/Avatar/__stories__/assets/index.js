42873cb4be0f23b6f4fd8dc3038b8ae515382e26
Kandio16 <kandio1604@gmail.com>
2021-11-10 10:25:30 +0200

    stories/add-icons

---
+++ src/components/Avatar/__stories__/assets/index.js
@@ -0,0 +1,11 @@
+import home from "./home.svg";
+import guest from "./guest.svg";
+import minus from "./minus.svg";
+import person1 from "./person1.png";
+import person2 from "./person2.png";
+import person3 from "./person3.png";
+import manIcon from "./manIcon.png";
+import womanIcon from "./womanIcon.png";
+import owner from "./owner.svg";
+
+export { home, guest, minus, person1, person2, person3, manIcon, womanIcon, owner };

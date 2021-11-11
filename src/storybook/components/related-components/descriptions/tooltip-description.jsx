ccfe07c6227dcdeda5b2ff3bd7548d7233425c7d
Kandio16 <kandio1604@gmail.com>
2021-11-11 13:20:09 +0200

    stories/fix

--- src/storybook/components/related-components/descriptions/tooltip-description.jsx
+++ src/storybook/components/related-components/descriptions/tooltip-description.jsx
@@ -3,7 +3,17 @@ import { RelatedComponent } from "../../related-component/related-component";
 import TooltipLineWrapper from "../../../../components/Tooltip/__stories__/TooltipLineWrapper";
 
 export const TooltipDescription = () => {
-  const component = useMemo(() => <TooltipLineWrapper />, []);
+  const style = {
+    marginTop: "40px"
+  };
+  const component = useMemo(
+    () => (
+      <div style={style}>
+        <TooltipLineWrapper justify="end" />
+      </div>
+    ),
+    []
+  );
   return (
     <RelatedComponent
       component={component}

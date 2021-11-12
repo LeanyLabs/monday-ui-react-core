2a53001bcf464f546b6855a3dad1f0a1fe7a1dbd
Kandio16 <kandio1604@gmail.com>
2021-11-12 12:19:31 +0200

    stories/add-RC

--- src/storybook/components/related-components/component-description-map.js
+++ src/storybook/components/related-components/component-description-map.js
@@ -6,6 +6,8 @@ import { LabelDescription } from "./descriptions/label-description";
 import { CounterDescription } from "./descriptions/counter-description";
 import { ChipsDescription } from "./descriptions/chips-description";
 import { TooltipDescription } from "./descriptions/tooltip-description";
+import { ToggleDescription } from "./descriptions/toggle-description";
+import { DropdownDescription } from "./descriptions/dropdown-description";
 
 export const SPLIT_BUTTON = "split-button";
 export const BUTTON_GROUP = "button-group";
@@ -15,6 +17,8 @@ export const RADIO_BUTTON = "radio-button";
 export const COUNTER = "counter";
 export const CHIPS = "chips";
 export const TOOLTIP = "tooltip";
+export const TOGGLE = "toggle";
+export const DROPDAWN = "dropdawn";
 
 export const descriptionTypesMap = new Map();
 
@@ -26,3 +30,5 @@ descriptionTypesMap.set(RADIO_BUTTON, <RadioButtonDescription />);
 descriptionTypesMap.set(COUNTER, <CounterDescription />);
 descriptionTypesMap.set(CHIPS, <ChipsDescription />);
 descriptionTypesMap.set(TOOLTIP, <TooltipDescription />);
+descriptionTypesMap.set(TOGGLE, <ToggleDescription />);
+descriptionTypesMap.set(DROPDAWN, <DropdownDescription />);
